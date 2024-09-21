8bfbbe32eb296ea680c52d62d326bbad2b82090e
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-09-21 12:55:17 -0700

    wip

--- src/OtpzServiceProvider.php
+++ src/OtpzServiceProvider.php
@@ -3,6 +3,12 @@
 namespace BenBjurstrom\Otpz;
 
 use BenBjurstrom\Otpz\Commands\OtpzCommand;
+use BenBjurstrom\Otpz\Http\Controllers\GetLoginController;
+use BenBjurstrom\Otpz\Http\Controllers\GetOtpController;
+use BenBjurstrom\Otpz\Http\Controllers\PostLoginController;
+use BenBjurstrom\Otpz\Http\Controllers\PostOtpController;
+use BenBjurstrom\Otpz\Http\Controllers\PostOtpLinkController;
+use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -21,5 +27,27 @@ class OtpzServiceProvider extends PackageServiceProvider
             ->hasViews('otpz')
             ->hasMigration('create_otps_table')
             ->hasCommand(OtpzCommand::class);
+
+        $this->registerOtpzRouteMacro();
+    }
+
+    protected function registerOtpzRouteMacro(): self
+    {
+        Route::macro('otpRoutes', function () {
+            Route::get('login', GetLoginController::class)->name('login');
+
+            Route::post('login', PostLoginController::class)->name('login.post');
+
+            Route::get('login/{id}', GetOtpController::class)
+                ->name('otp.show');
+
+            Route::post('login/{id}', PostOtpController::class)
+                ->name('otp.post');
+
+            Route::get('otplink', PostOtpLinkController::class)
+                ->name('otplink');
+        });
+
+        return $this;
     }
 }
